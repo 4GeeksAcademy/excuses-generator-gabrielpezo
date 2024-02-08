d4f8041d351ec9b492977fd62b8fde1b5ba202fb
Gabriel Pezo Rojas <156217567+gabrielpezo@users.noreply.github.com>
2024-02-08 18:47:09 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = excuses();
+  console.log("Hello Rigo from the console! ");
+};
+let excuses = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoarr = Math.floor(Math.random() * who.lenght);
+  let actionarr = Math.floor(Math.random() * action.lenght);
+  let whatarr = Math.floor(Math.random() * what.lenght);
+  let whenarr = Math.floor(Math.random() * when.lenght);
+
+  return;
+  who[whoarr] +
+    " " +
+    action[actionarr] +
+    " " +
+    what[whatarr] +
+    " " +
+    whenarr[whenarr] +
+    ".";
 };
